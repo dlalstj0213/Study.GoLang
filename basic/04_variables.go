6dbc010c60a48f25de7d3d9d6fe830be5c430386
MinseoRhie <dlalstj0213@gmail.com>
2023-08-05 06:54:59 +0900

    study(variables): var 작성 방법

---
+++ basic/04_variables.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+var name, gender string
+var age int
+var isStudent bool
+
+func main() {
+	var i int
+	fmt.Println("i:", i)
+	fmt.Println("name:", name)
+	fmt.Println("gender:", gender)
+	fmt.Println("age:", age)
+	fmt.Println("isStudent:", isStudent)
+}
+
+/*
+- var 문은 변수에 대한 목록을 선언한다. 함수 매개변수 작성법과 동일하게 마지막은 type이다.
+
+- var 문은 package나 함수 단에 존재 할 수 있다.
+*/
